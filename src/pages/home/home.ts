2838ac6d6c955d26b138d90726df7bc0d968a0ab
Arek Ruta <arek.ruta89@gmail.com>
2017-07-29 04:23:02 +0200

    DestinationType.FILE_URI and DATA_URL

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Camera, CameraOptions } from "@ionic-native/camera";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  public base64Image: string = null;
+  imgSrc: any;
+  public photos: any;
+  constructor(public navCtrl: NavController, private camera: Camera) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ngOnInit() {
+    this.photos = [];
   }
 
+  takePicture() {
+    const options: CameraOptions = {
+      targetWidth: 500,
+      targetHeight: 500,
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        // this.base64Image = "data:image/jpeg;base64," + imageData;
+        this.photos.push(imageData);
+        this.photos.reverse();
+        alert(imageData);
+      },
+      err => {
+        // Handle error
+        alert(err);
+      }
+    );
+  }
+
+    takePicture2() {
+    const options: CameraOptions = {
+      targetWidth: 500,
+      targetHeight: 500,
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+       
+        alert(this.base64Image);
+      },
+      err => {
+        // Handle error
+        alert(err);
+      }
+    );
+  }
+
+  deletePhoto(id){
+    alert(id)
+  }
 }
